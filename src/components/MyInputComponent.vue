e39bf2075f4d79409b9add1422d8d52cec891b1b
edccc <1742648313@qq.com>
2020-11-16 15:02:48 +0800

    添加input子组件，并且父组件给子组件传值

---
+++ src/components/MyInputComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <!-- 用属性的方式接收父组件传过来的数据 -->
+    <input type="text" :placeholder="title" />
+    <button>提交信息</button>
+  </div>
+</template>
+
+<script>
+export default {
+  // 接收父组件的传递信息
+  // 用prop注册接收
+  props: ["title"],
+};
+</script>
+
+<style>
+</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
     <h1>这是一个登录页面</h1>
+    <!-- 主动给子组件发送数据 用:属性方式从data数据里面读取数据 -->
+    <!-- <Myinput :title="msg" /> -->
+    <!-- <Myinput :title="passWord" /> -->
+    <!-- <Myinput :title="nickName" /> -->
+    <!-- 第二种直接赋值 -->
+    <Myinput title="请输入用户名" />
+    <Myinput title="请输入密码" />
+    <Myinput title="请输入昵称" />
     <Logo />
   </div>
 </template>
@@ -8,9 +16,19 @@
 <script>
 // 导入组件
 import Logo from "../components/Logo";
+// 导入子组件,Myinput为自定义组件名称, from为导入组件的路径
+import Myinput from "../components/MyInputComponent";
 export default {
+  data() {
+    return {
+      msg: "请输入用户名",
+      passWord: "请输入密码",
+      nickName: "请输入昵称",
+    };
+  },
   components: {
     Logo,
+    Myinput,
   },
 };
 </script>
