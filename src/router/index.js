5bebb5c6c8fc056521248d4f5bb0b200478e36dd
edccc <1742648313@qq.com>
2020-11-20 15:04:03 +0800

    增加全局路由守卫

--- src/router/index.js
+++ src/router/index.js
@@ -44,4 +44,20 @@ const router = new VueRouter({
   routes
 })
 
+// 添加全局路由守卫
+// 拦截所有跳转，执行回调函数
+router.beforeEach((to, from, next) => {
+  // 如果跳转到个人中心页面
+  if (to.path == "/userindex") {
+    // 判断有无token再放行
+    if (localStorage.getItem('token')) {
+      // 则执行next()
+      return next()
+    } else {
+      // 否则就跳转到登录页面
+      return router.push("/login")
+    }
+  }
+  return next()
+})
 export default router
