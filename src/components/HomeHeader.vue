2488ef73e0b93e25cdf33db1994179accd85bede
edccc <1742648313@qq.com>
2020-11-24 14:47:51 +0800

    封装首页头部组件和首页的栏目分类管理

---
+++ src/components/HomeHeader.vue
@@ -0,0 +1,58 @@
+<template>
+  <header>
+    <!-- logo -->
+    <span class="iconfont iconnew"></span>
+    <div class="input">
+      <span class="iconfont iconsearch"></span>
+      <em>搜索新闻</em>
+    </div>
+    <span
+      class="iconfont iconwode"
+      @click="$router.push('../userindex')"
+    ></span>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+<style lang="less" scoped>
+header {
+  width: 100%;
+  height: 49/360 * 100vw;
+  background-color: #ff0000;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  .iconnew {
+    font-size: 54/360 * 100vw;
+    color: #fff;
+  }
+  .input {
+    width: 210/360 * 100vw;
+    height: 34/360 * 100vw;
+    outline: none;
+    border: none;
+    border-radius: 17/360 * 100vw;
+    background-color: rgba(255, 255, 255, 0.4);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #fff;
+    .iconfont {
+      font-size: 14/360 * 100vw;
+      margin-top: 3/360 * 100vw;
+      margin-right: 5/360 * 100vw;
+    }
+    em {
+      font-style: normal;
+      font-size: 14/360 * 100vw;
+    }
+  }
+  .iconwode {
+    font-size: 32/360 * 100vw;
+    color: #fff;
+    opacity: 0.6;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,94 @@
 <template>
-  <div id="app">
-    <router-link to="/login">登录</router-link> |
-    <router-link to="/register">注册</router-link>
+  <div class="content">
+    <!-- 头部导航栏 -->
+    <Homeheader />
+    <!-- 栏目管理 -->
+    <van-tabs
+      v-model="active"
+      swipeable
+      background="#e4e4e4"
+      @click="setSelect"
+    >
+      <van-tab
+        v-for="(item, index) in content"
+        :title="item.name"
+        :name="item.id"
+        :key="index"
+      >
+        内容 {{ item.name }}
+      </van-tab>
+      <span class="iconfont iconjiantou1"></span>
+    </van-tabs>
   </div>
 </template>
 
 <script>
-export default {};
+import Homeheader from "../components/HomeHeader";
+export default {
+  components: {
+    Homeheader,
+  },
+  data() {
+    return {
+      content: [],
+      active: "",
+    };
+  },
+  created() {
+    this.$axios({
+      url: "/category",
+    }).then((res) => {
+      res.data.data.forEach((item) => {
+        // console.log(item);
+        if (item.is_top === 0) {
+          this.content.push(item);
+        }
+      });
+      console.log(this.content);
+    });
+    this.$axios({
+      mesthod: "post",
+      url: "/post",
+    }).then((res) => {
+      console.log(res);
+    });
+  },
+  methods: {
+    setSelect(name, title) {
+      console.log(name);
+    },
+  },
+};
 </script>
 
-<style>
-a {
-  color: black;
-  text-decoration: none;
+<style lang="less" scoped>
+.content {
+  // 栏目管理
+  /deep/ .van-tabs__wrap--scrollable {
+    height: 43/360 * 100vw;
+    .van-tab {
+      .van-tab__text {
+        font-size: 16/360 * 100vw;
+        // line-height: 43/360 * 100vw;
+      }
+      &:last-child {
+        margin-right: 42/360 * 100vw;
+      }
+    }
+  }
+  .iconjiantou1 {
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: 42/360 * 100vw;
+    height: 43/360 * 100vw;
+    background-color: #e4e4e4;
+    transform: rotate(90deg);
+    font-size: 25/360 * 100vw;
+    font-weight: 700;
+    line-height: 42/360 * 100vw;
+    text-align: center;
+    z-index: 999;
+  }
 }
 </style>
