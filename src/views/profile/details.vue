73a550e7993bfc76a96b0bca7899118374b45116
edccc <1742648313@qq.com>
2020-11-27 15:25:00 +0800

    动态根据关注状态和点赞状态添加类名

--- src/views/profile/details.vue
+++ src/views/profile/details.vue
@@ -1,30 +1,47 @@
 <template>
   <div>
-    <div class="topnav">
-      <div class="icon">
-        <span class="iconfont iconjiantou2" @click="$router.back()"></span>
-        <span class="iconfont iconnew"></span>
-      </div>
-      <div v-if="details.has_follow == false" class="button" @click="getFollow">
-        关注
-      </div>
-      <div v-else class="button active" @click="getUnfollow">已关注</div>
-    </div>
-    <div class="details">
-      <div class="title">{{ details.title }}</div>
-      <div class="info">{{ details.user.nickname }} 2019-10-10</div>
-      <div class="content" v-html="details.content"></div>
-      <div class="icon">
-        <div class="dianzan" @click="getLike">
-          <span
-            v-if="details.has_like == true"
-            class="iconfont icondianzan active"
-          ></span>
-          <span v-else class="iconfont icondianzan"></span>
-          {{ details.like_length }}
+    <!-- 图片新闻 -->
+    <div class="nomalPost">
+      <div class="topnav">
+        <div class="icon">
+          <span class="iconfont iconjiantou2" @click="$router.back()"></span>
+          <span class="iconfont iconnew"></span>
         </div>
-        <div class="weixin"><span class="iconfont iconweixin"></span>微信</div>
+        <div
+          @click="goStar"
+          class="button"
+          :class="{ active: details.has_follow }"
+        >
+          {{ details.has_follow ? "已关注" : "关注" }}
+        </div>
       </div>
+      <div class="details">
+        <!-- 图片文章 -->
+        <div class="img" v-if="details.type == 1">
+          <div class="title">{{ details.title }}</div>
+          <div class="info">{{ details.user.nickname }} 2019-10-10</div>
+          <div class="content" v-html="details.content"></div>
+        </div>
+        <!-- 视频文章 -->
+        <!-- <div class="video">
+        <video :src="details.content"></video>
+      </div> -->
+      </div>
+    </div>
+    <!-- 视频新闻 -->
+    <div class="videoNews">
+      <video src=""></video>
+    </div>
+    <div class="icon">
+      <div class="dianzan" @click="getLike">
+        <span
+          class="iconfont icondianzan"
+          :class="{ active: details.has_like }"
+        ></span>
+        <!-- <span  class="iconfont icondianzan"></span> -->
+        {{ details.like_length }}
+      </div>
+      <div class="weixin"><span class="iconfont iconweixin"></span>微信</div>
     </div>
     <sendComment @star="getStar" />
   </div>
@@ -59,30 +76,33 @@ export default {
         });
       });
     },
+    // 关注按钮
+    goStar() {
+      if (this.details.has_follow == false) {
+        this.getFollow();
+        this.details.has_follow = true;
+      } else {
+        this.getUnfollow();
+        this.details.has_follow = false;
+      }
+    },
     //   关注事件
     getFollow() {
       this.$axios({
         url: "/user_follows/" + this.details.user.id,
-      }).then((res) => {
-        console.log(res);
-        this.Loadpage();
-      });
+      }).then((res) => {});
     },
     // 取消关注
     getUnfollow() {
       this.$axios({
         url: "/user_unfollow/" + this.details.user.id,
-      }).then((res) => {
-        console.log(res);
-        this.Loadpage();
-      });
+      }).then((res) => {});
     },
     // 点赞事件
     getLike() {
       this.$axios({
         url: "/post_like/" + this.details.id,
       }).then((res) => {
-        // console.log(res);
         this.$toast(res.data.message);
         this.Loadpage();
       });
@@ -92,7 +112,7 @@ export default {
       this.$axios({
         url: "/post_star/" + this.details.id,
       }).then((res) => {
-        console.log(res);
+        // console.log(res);
         this.$toast(res.data.message);
       });
     },
@@ -101,82 +121,85 @@ export default {
 </script>
 
 <style lang="less" scoped>
-.topnav {
-  padding: 16/360 * 100vw;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  .icon {
+.nomalPost {
+  .topnav {
+    padding: 16/360 * 100vw;
     display: flex;
+    justify-content: space-between;
     align-items: center;
-    .iconjiantou2 {
+    .icon {
+      display: flex;
+      align-items: center;
+      .iconjiantou2 {
+        font-size: 12/360 * 100vw;
+        margin-right: 8/360 * 100vw;
+      }
+      .iconnew {
+        font-size: 54/360 * 100vw;
+      }
+    }
+    .button {
+      width: 62/360 * 100vw;
+      height: 26/360 * 100vw;
+      background-color: #ff0000;
+      border-radius: 13/360 * 100vw;
       font-size: 12/360 * 100vw;
-      margin-right: 8/360 * 100vw;
+      text-align: center;
+      line-height: 26/360 * 100vw;
+      color: #fff;
     }
-    .iconnew {
-      font-size: 54/360 * 100vw;
+    .active {
+      background-color: #f2f2f2;
+      color: #333;
     }
   }
-  .button {
-    width: 62/360 * 100vw;
-    height: 26/360 * 100vw;
-    background-color: #ff0000;
-    border-radius: 13/360 * 100vw;
-    font-size: 12/360 * 100vw;
-    text-align: center;
-    line-height: 26/360 * 100vw;
-    color: #fff;
-  }
-  .active {
-    background-color: #f2f2f2;
-    color: #333;
+  .details {
+    padding: 0 16/360 * 100vw;
+    .title {
+      font-size: 18/360 * 100vw;
+      font-weight: 700;
+    }
+    .info {
+      margin: 10/360 * 100vw 0;
+      font-size: 13/360 * 100vw;
+      color: #868686;
+    }
+    /deep/.content {
+      margin: 10/360 * 100vw 0;
+      font-size: 14/360 * 100vw;
+      color: #333;
+      img {
+        width: 100%;
+        // height: 340/360 * 100vw;
+        object-fit: cover;
+      }
+    }
   }
 }
-.details {
-  padding: 0 16/360 * 100vw;
-  .title {
-    font-size: 18/360 * 100vw;
-    font-weight: 700;
-  }
-  .info {
-    margin: 10/360 * 100vw 0;
+
+.icon {
+  display: flex;
+  justify-content: space-evenly;
+  margin: 30/360 * 100vw 0 20/360 * 100vw;
+  div {
+    display: block;
+    // padding: 0 30/360 * 100vw;
+    width: 79/360 * 100vw;
+    height: 29/360 * 100vw;
+    border: 1px solid #e4e4e4;
     font-size: 13/360 * 100vw;
-    color: #868686;
-  }
-  /deep/.content {
-    margin: 10/360 * 100vw 0;
-    font-size: 14/360 * 100vw;
-    color: #333;
-    img {
-      width: 100%;
-      // height: 340/360 * 100vw;
-      object-fit: cover;
+    text-align: center;
+    line-height: 29/360 * 100vw;
+    border-radius: 15/360 * 100vw;
+    span {
+      margin-right: 5/360 * 100vw;
+      font-size: 17/360 * 100vw;
     }
-  }
-  .icon {
-    display: flex;
-    justify-content: space-evenly;
-    margin: 30/360 * 100vw 0 20/360 * 100vw;
-    div {
-      display: block;
-      // padding: 0 30/360 * 100vw;
-      width: 79/360 * 100vw;
-      height: 29/360 * 100vw;
-      border: 1px solid #e4e4e4;
-      font-size: 13/360 * 100vw;
-      text-align: center;
-      line-height: 29/360 * 100vw;
-      border-radius: 15/360 * 100vw;
-      span {
-        margin-right: 5/360 * 100vw;
-        font-size: 17/360 * 100vw;
-      }
-      .active {
-        color: #ff0000;
-      }
-      .iconweixin {
-        color: #01c801;
-      }
+    .active {
+      color: #ff0000;
+    }
+    .iconweixin {
+      color: #01c801;
     }
   }
 }
