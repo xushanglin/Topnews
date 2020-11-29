c3d1bcf97d4ae1997a50369e1b46c9eebce5969a
edccc <1742648313@qq.com>
2020-11-29 14:51:20 +0800

    搜索记录保存到本地，监听搜索记录得改变添加到本地

---
+++ src/views/profile/search.vue
@@ -0,0 +1,109 @@
+<template>
+  <div>
+    <div class="topNav">
+      <span class="iconfont iconjiantou2" @click="$router.back()"></span>
+      <div class="search">
+        <input
+          type="text"
+          v-model="record"
+          placeholder="给你好看得"
+          @keyup.enter="gogogo()"
+        />
+      </div>
+      <div class="button">搜索</div>
+    </div>
+    <div class="history">
+      <div class="tip">历史记录</div>
+      <ul>
+        <li v-for="list in historyList" :key="list.index">{{ list }}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      record: "",
+      historyList: [],
+    };
+  },
+  // 挂载好就显示历史记录
+  created() {
+    // 如果有就显示
+    if (localStorage.getItem("history")) {
+      this.historyList = JSON.parse(localStorage.getItem("history"));
+    }
+  },
+  watch: {
+    historyList() {
+      // console.log(this.historyList);
+      // 监听historyLIst的变化给他添加到本地
+      localStorage.setItem("history", JSON.stringify(this.historyList));
+    },
+  },
+  methods: {
+    gogogo() {
+      if (this.historyList.indexOf(this.record) == -1) {
+        // 如果历史记录没有这条记录，给他添加
+        this.historyList.unshift(this.record);
+      }
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.topNav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 15/360 * 100vw;
+  .iconfont {
+    font-size: 15/360 * 100vw;
+  }
+  .button {
+    font-size: 14/360 * 100vw;
+    color: #333;
+  }
+  .search {
+    margin: 0 20/360 * 100vw;
+    flex-grow: 1;
+    position: relative;
+    display: flex;
+    align-items: center;
+    input {
+      padding-left: 15/360 * 100vw;
+      border: 1px solid #ccc;
+      width: 100%;
+      height: 38/360 * 100vw;
+      border-radius: 19/360 * 100vw;
+      color: #ccc;
+      &::placeholder {
+        padding-left: 40/360 * 100vw;
+        font-size: 13/360 * 100vw;
+      }
+    }
+  }
+}
+.history {
+  padding: 15/360 * 100vw;
+  border-bottom: 1px solid #eee;
+}
+.tip {
+  font-weight: 700;
+  font-size: 14/360 * 100vw;
+  color: #333;
+}
+ul {
+  display: flex;
+  flex-wrap: wrap;
+  li {
+    width: 50%;
+    font-size: 13/360 * 100vw;
+    color: #333;
+    margin-top: 10/360 * 100vw;
+  }
+}
+</style>
