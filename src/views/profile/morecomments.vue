34ab1584e0a877bafd36290ad89ac720efc2681d
edccc <1742648313@qq.com>
2020-11-30 17:12:22 +0800

    评论翻页组件

--- src/views/profile/morecomments.vue
+++ src/views/profile/morecomments.vue
@@ -1,11 +1,19 @@
 <template>
   <div>
     <Header text="更多跟帖" />
-    <Maincomment
-      v-for="comment in commentList"
-      :key="comment.id"
-      :commentData="comment"
-    />
+
+    <van-list
+      v-model="loading"
+      :finished="finished"
+      finished-text="没有更多了"
+      @load="onLoad"
+    >
+      <Maincomment
+        v-for="comment in commentList"
+        :key="comment.id"
+        :commentData="comment"
+      />
+    </van-list>
   </div>
 </template>
 
@@ -16,19 +24,40 @@ export default {
   data() {
     return {
       commentList: [],
+      pageIndex: 1,
+      pageSize: 5,
+      loading: false,
+      finished: false,
     };
   },
   components: {
     Header,
     Maincomment,
   },
+  methods: {
+    onLoad() {
+      this.pageIndex++;
+      this.loadPage();
+    },
+    loadPage() {
+      this.$axios({
+        url: "/post_comment/" + this.$route.params.id,
+        params: {
+          pageIndex: this.pageIndex,
+          pageSize: this.pageSize,
+        },
+      }).then((res) => {
+        //   console.log(res);
+        this.commentList = [...this.commentList, ...res.data.data];
+        this.loading = false;
+        if (res.data.data.length < this.pageSize) {
+          this.finished = true;
+        }
+      });
+    },
+  },
   created() {
-    this.$axios({
-      url: "/post_comment/" + this.$route.params.id,
-    }).then((res) => {
-      //   console.log(res);
-      this.commentList = res.data.data;
-    });
+    this.loadPage();
   },
 };
 </script>
