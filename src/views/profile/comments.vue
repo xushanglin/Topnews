79a93d00792ae777e99748308a533859d14c9218
edccc <1742648313@qq.com>
2020-12-01 15:08:18 +0800

    我的跟帖页面，删除我的跟帖组件

--- src/views/profile/comments.vue
+++ src/views/profile/comments.vue
@@ -4,10 +4,21 @@
     <!-- 跟帖详情 -->
     <div class="comments" v-for="(item, index) in comments" :key="index">
       <p class="time">2019-10-10 10:25</p>
-      <mycomment :comment="item.parent" v-if="item.parent" />
+      <div class="parent" v-if="item.parent">
+        <div class="nickname">回复：{{ item.parent.user.nickname }}</div>
+        <div class="content">{{ item.parent.content }}</div>
+      </div>
       <p class="message">{{ item.content }}</p>
-      <p class="text" @click="$router.push('/details/' + item.post.id)">
+      <p
+        class="text"
+        @click="$router.push('/details/' + item.post.id)"
+        v-if="item.post.title"
+      >
         <span class="title">{{ item.post.title }}</span>
+        <span class="iconfont iconjiantou1"></span>
+      </p>
+      <p class="text" v-else>
+        <span class="title">此贴已被删除</span>
         <span class="iconfont iconjiantou1"></span>
       </p>
     </div>
@@ -16,11 +27,9 @@
 
 <script>
 import Header from "@/components/Header";
-import mycomment from "../../components/mycomment";
 export default {
   components: {
     Header,
-    mycomment,
   },
   data() {
     return {
@@ -48,6 +57,20 @@ export default {
     margin-bottom: 20/360 * 100vw;
     font-size: 14/360 * 100vw;
     color: #797979;
+  }
+  .parent {
+    background-color: #e4e4e4;
+    padding: 16/360 * 100vw 10/360 * 100vw;
+    border-radius: 10/360 * 100vw;
+    .nickname {
+      font-size: 12/360 * 100vw;
+      margin-bottom: 10/360 * 100vw;
+      color: #868686;
+    }
+    .content {
+      font-size: 14/360 * 100vw;
+      color: #868686;
+    }
   }
   .message {
     font-size: 14/360 * 100vw;
